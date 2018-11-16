6c233dc69ebba021d479afc751d8e4f21082049a
StrikerSK <thestrikersk@gmail.com>
2018-11-16 18:40:58 +0100

    Updating rest-consumer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, {PureComponent} from 'react';
+import Link from 'next/link';
+import {BrowserRouter, NavLink, Route, Switch, Redirect} from "react-router-dom";
 
-class App extends Component {
+import Student from "./Components/Student";
+import Welcome from "./Components/Welcome";
+import WelcomePage from "./Components/WelcomePage";
 
+class App extends PureComponent {
     constructor(props) {
         super(props);
         this.state = {
@@ -11,16 +15,34 @@ class App extends Component {
     }
 
     componentDidMount() {
-        fetch("http://localhost:8080/REST/getStudents")
+        fetch("https://springhelloworldapp.herokuapp.com/REST/getStudents")
+        // fetch("http://localhost:8080/REST/getStudent s")
             .then(response => response.json())
             .then((result) => this.setState({ students: result }))
     }
 
-    render() {
-        return (
-            <ul>{this.state.students.map(item => <li key={item.id}><strong>{item.firstName} {item.lastName}</strong> from <strong>{item.country}</strong></li>)}</ul>
-        );
-    }
+  render() {
+    return (
+      <BrowserRouter>
+          <div className="App">
+              <h4>Student list:</h4>
+              <ul className="student-links">
+                  {this.state.students.map(item => (
+                      <li key={item.id}><NavLink to={`/student/${item.id}`}>{item.firstName} {item.lastName}</NavLink></li>
+                  ))}
+              </ul>
+
+              <Switch>
+                  <Route exact strict path="/" component={WelcomePage}/>
+                  <Route exact strict path="/student/:studentId" component={Student}/>
+                  <Route exact strict path="/welcome/:name" component={Welcome}/>
+                  <Redirect to="/"/>
+              </Switch>
+
+          </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
---
+++ src/Components/Student.js
@@ -0,0 +1,70 @@
+import React from "react";
+
+// const API = `http://localhost:8080/REST/getStudent?id=`;
+const API = `https://springhelloworldapp.herokuapp.com/REST/getStudent?id=`;
+
+class Student extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            student: [],
+            knownLanguages: [],
+            spokenLanguages: []
+        }
+    }
+
+    componentDidMount() {
+        const NUM = this.props.match.params.studentId;
+
+        fetch(API + NUM)
+            .then(response => response.json())
+            .then((result) => this.setState({ student: result, knownLanguages: result.knownLanguages, spokenLanguages: result.spokenLanguages }));
+    }
+
+    componentWillReceiveProps(props) {
+        const studentId = props.match.params.studentId;
+
+        fetch(API + studentId)
+            .then(response => response.json())
+            .then((result) => this.setState({ student: result, knownLanguages: result.knownLanguages, spokenLanguages: result.spokenLanguages }));
+    }
+
+    static listLanguages(langugage){
+        if(langugage.length === 0){
+            return (<li>No language!</li>)
+        } else {
+            return langugage.map(item => <li>{item}</li>)
+        }
+    }
+
+    render() {
+        console.log(this.state.student);
+
+        return (
+            <div>
+                <div className={"student-details"}>
+                    <h1>{this.state.student.firstName} {this.state.student.lastName}</h1>
+                    <p>Country of origin: <strong>{this.state.student.country}</strong></p>
+                    <p>Type of study: <strong>{this.state.student.typeOfStudy}</strong></p>
+                    <p>Grade of study: <strong>{this.state.student.grade}</strong></p>
+                </div>
+                <div className="row">
+                    <div className="column">
+                        <ul>
+                            <lh><strong>Programming languages</strong></lh>
+                            {Student.listLanguages(this.state.knownLanguages)}
+                        </ul>
+                    </div>
+                    <div className="column">
+                        <ul>
+                            <lh><strong>Spoken languages</strong></lh>
+                            {Student.listLanguages(this.state.spokenLanguages)}
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Student;
